2ae15c2fb2033fe15d00509a0cc126ed3fec8cff
sandimaulidika <sandimaulidika@gmail.com>
2025-06-11 10:58:30 +0700

    Refactor deployment script to streamline permission settings and optimize dependency installation; update logo paths and add .gitignore files for storage directories

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -2,6 +2,6 @@ import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
     return (
-        <img src="/storage/logo.webp" alt="" />
+        <img src="/logo.webp" alt="Logo Sans Store" />
     );
 }
